32580e2395f75712e0e0c579c961a8de0d999ab4
Saujanya00 <140233156+Saujanya00@users.noreply.github.com>
2023-08-03 12:11:52 +0530

    Create cicd.groovy
    Writing functions for the first 3 stages - Download, Build, Deployment

---
+++ lib/cicd.groovy
@@ -0,0 +1,12 @@
+def gitDownload(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def mavenBuild()
+{
+  sh 'mvn package'
+}
+def tomcatDeploy(ip,context)
+{
+  deploy adapters: [tomcat9(credentialsId: '00ba1864-f667-4649-b5fb-bbc5955ebc83', path: '', url: '${ip}')], contextPath: '${context}', war: '**/*.war'
+}
